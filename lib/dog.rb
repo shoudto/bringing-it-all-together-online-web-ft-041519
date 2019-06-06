718b1b6a253a5ae75d33de10bd03b2bea7565f25
Oscar Shoudt <shoudto@gmail.com>
2019-06-06 01:25:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,24 @@ class Dog
     @name = name 
     @breed = breed 
   end 
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT 
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end 
 end 

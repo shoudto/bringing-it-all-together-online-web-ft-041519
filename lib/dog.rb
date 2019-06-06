bcdd15d7cc0b511473b7048f4ba29944c5c5b5ff
Oscar Shoudt <shoudto@gmail.com>
2019-06-06 02:06:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,5 +61,11 @@ class Dog
   def self.new_from_db(row)
     new_dog = self.new(id: row[0], name: row[1], breed: row[2])
     new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end 
 end 
